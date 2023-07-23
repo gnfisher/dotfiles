f0d10d3b6a8e72629c98ae96063d27935be93160
Greg Fisher <greg@gnfisher.com>
2023-07-23 19:57:49 -0400

    Don't set the clipboard to rdm unless its a codespace

--- config/nvim/lua/base/rdm.lua
+++ config/nvim/lua/base/rdm.lua
@@ -1,4 +1,5 @@
-if vim.fn.executable('rdm') == 1 then
+-- We want to make sure we are in a codespace and rdm is installed.
+if vim.fn.executable('rdm') == 1 and os.getenv("CODESPACES") ~= nil then
   vim.g.clipboard = {
     name = "rdm",
     copy = {
