fd173fc5e0a0fcc95a07294aaa28cbe1b70c6ad1
Greg Fisher <greg@gnfisher.com>
2023-07-24 15:37:02 -0400

    Install.sh tree-sitter

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ if [[ "$CODESPACES" = "true" ]]; then
   gh config set browser "rdm open"
 
   export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH
-  brew install neovim starship typescript-language-server vscode-langservers-extracted golangci-lint
+  brew install neovim starship typescript-language-server vscode-langservers-extracted golangci-lint tree-sitter
 
   if [[ -d /etc/ssh ]]; then
     echo 'AcceptEnv TZ LC_*' >> /etc/ssh/sshd_config
