016fcae75a66cea2103972738b1225c46ef1db19
Greg Fisher <greg@gnfisher.com>
2023-08-18 11:35:56 -0400

    Github color

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -122,50 +122,50 @@ require('lazy').setup({
     },
   },
 
-  -- {
-  --   'RRethy/nvim-base16',
-  --   config = function()
-  --     vim.cmd.colorscheme 'base16-windows-95-light'
-  --     vim.o.background = 'light'
-  --   end
-  -- },
-
   {
-    -- Colorscheme and config
-    'NTBBloodbath/doom-one.nvim',
-    priority = 1000,
+    'projekt0n/github-nvim-theme',
     config = function()
-      vim.cmd.colorscheme 'doom-one'
-      vim.g.doom_one_cursor_coloring = true
-      -- Set :terminal colors
-      vim.g.doom_one_terminal_colors = true
-      -- Enable italic comments
-      vim.g.doom_one_italic_comments = true
-      -- Enable TS support
-      vim.g.doom_one_enable_treesitter = true
-      -- Color whole diagnostic text or only underline
-      vim.g.doom_one_diagnostics_text_color = true
-      -- Enable transparent background
-      vim.g.doom_one_transparent_background = false
-
-      -- Pumblend transparency
-      vim.g.doom_one_pumblend_enable = false
-      vim.g.doom_one_pumblend_transparency = 20
-
-      -- Plugins integration
-      vim.g.doom_one_plugin_neorg = true
-      vim.g.doom_one_plugin_barbar = false
-      vim.g.doom_one_plugin_telescope = false
-      vim.g.doom_one_plugin_neogit = true
-      vim.g.doom_one_plugin_nvim_tree = true
-      vim.g.doom_one_plugin_dashboard = true
-      vim.g.doom_one_plugin_startify = true
-      vim.g.doom_one_plugin_whichkey = true
-      vim.g.doom_one_plugin_indent_blankline = true
-      vim.g.doom_one_plugin_vim_illuminate = true
-      vim.g.doom_one_plugin_lspsaga = false
-    end,
+      vim.cmd.colorscheme 'github_dark'
+      vim.o.background = 'dark'
+    end
   },
+
+  -- {
+  --   -- Colorscheme and config
+  --   'NTBBloodbath/doom-one.nvim',
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme 'doom-one'
+  --     vim.g.doom_one_cursor_coloring = true
+  --     -- Set :terminal colors
+  --     vim.g.doom_one_terminal_colors = true
+  --     -- Enable italic comments
+  --     vim.g.doom_one_italic_comments = true
+  --     -- Enable TS support
+  --     vim.g.doom_one_enable_treesitter = true
+  --     -- Color whole diagnostic text or only underline
+  --     vim.g.doom_one_diagnostics_text_color = true
+  --     -- Enable transparent background
+  --     vim.g.doom_one_transparent_background = false
+
+  --     -- Pumblend transparency
+  --     vim.g.doom_one_pumblend_enable = false
+  --     vim.g.doom_one_pumblend_transparency = 20
+
+  --     -- Plugins integration
+  --     vim.g.doom_one_plugin_neorg = true
+  --     vim.g.doom_one_plugin_barbar = false
+  --     vim.g.doom_one_plugin_telescope = false
+  --     vim.g.doom_one_plugin_neogit = true
+  --     vim.g.doom_one_plugin_nvim_tree = true
+  --     vim.g.doom_one_plugin_dashboard = true
+  --     vim.g.doom_one_plugin_startify = true
+  --     vim.g.doom_one_plugin_whichkey = true
+  --     vim.g.doom_one_plugin_indent_blankline = true
+  --     vim.g.doom_one_plugin_vim_illuminate = true
+  --     vim.g.doom_one_plugin_lspsaga = false
+  --   end,
+  -- },
 
   {
     -- Set lualine as statusline
@@ -300,7 +300,7 @@ vim.o.clipboard = 'unnamedplus'
 require 'base.rdm'
 
 -- Where am I?
-vim.o.cursorcolumn = true
+-- vim.o.cursorcolumn = true
 vim.o.cursorline = true
 
 -- Enable break indent
