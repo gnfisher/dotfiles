07c559dec7f938aabe0390fb30f856875a3a18ba
Greg Fisher <greg@gnfisher.com>
2023-08-20 22:10:22 -0400

    neorg maybe

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -133,43 +133,6 @@ require('lazy').setup({
     end
   },
 
-  -- {
-  --   -- Colorscheme and config
-  --   'NTBBloodbath/doom-one.nvim',
-  --   priority = 1000,
-  --   config = function()
-  --     vim.cmd.colorscheme 'doom-one'
-  --     vim.g.doom_one_cursor_coloring = true
-  --     -- Set :terminal colors
-  --     vim.g.doom_one_terminal_colors = true
-  --     -- Enable italic comments
-  --     vim.g.doom_one_italic_comments = true
-  --     -- Enable TS support
-  --     vim.g.doom_one_enable_treesitter = true
-  --     -- Color whole diagnostic text or only underline
-  --     vim.g.doom_one_diagnostics_text_color = true
-  --     -- Enable transparent background
-  --     vim.g.doom_one_transparent_background = false
-
-  --     -- Pumblend transparency
-  --     vim.g.doom_one_pumblend_enable = false
-  --     vim.g.doom_one_pumblend_transparency = 20
-
-  --     -- Plugins integration
-  --     vim.g.doom_one_plugin_neorg = true
-  --     vim.g.doom_one_plugin_barbar = false
-  --     vim.g.doom_one_plugin_telescope = false
-  --     vim.g.doom_one_plugin_neogit = true
-  --     vim.g.doom_one_plugin_nvim_tree = true
-  --     vim.g.doom_one_plugin_dashboard = true
-  --     vim.g.doom_one_plugin_startify = true
-  --     vim.g.doom_one_plugin_whichkey = true
-  --     vim.g.doom_one_plugin_indent_blankline = true
-  --     vim.g.doom_one_plugin_vim_illuminate = true
-  --     vim.g.doom_one_plugin_lspsaga = false
-  --   end,
-  -- },
-
   {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
@@ -238,6 +201,12 @@ require('lazy').setup({
 
       vim.g["test#strategy"] = "vtr"
     end
+  },
+
+  {
+    'nvim-neorg/neorg',
+    config = function()
+    end,
   },
 
   -- Installs nvim-metals and sets it up for Scala.
@@ -809,6 +778,26 @@ cmp.setup {
     }
   },
 }
+
+require("neorg").setup {
+  load = {
+    ["core.defaults"] = {},
+    ["core.concealer"] = {},
+    ["core.journal"] = {
+      workspace = "personal"
+    },
+    ["core.dirman"] = {
+      config = {
+        workspaces = {
+          work = "~/notes/work",
+          zettl = "~/notes/zettl",
+          personal = "~/notes/personal",
+        }
+      }
+    }
+  }
+}
+-- vim.api.nvim_command('Neorg sync-parsers')
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
