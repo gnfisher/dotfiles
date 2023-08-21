8a242ad42ae036cc81e712a91dc9e2519000a460
Greg Fisher <greg@gnfisher.com>
2023-08-20 22:45:02 -0400

    It was too fast

--- install.sh
+++ install.sh
@@ -26,8 +26,8 @@ elif [[ "$(uname)" = "Darwin" ]]; then
   rcup -v
 
   # Must.. go.. faster..
-  defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
-  defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+  # defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
+  # defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
 else
   >&2 echo "error: Unknown system"
   exit 1
