d2c3087a2551a2151ba55971a46aad0c5c7d220a
Greg Fisher <greg@gnfisher.com>
2024-04-27 19:14:18 -0400

    Cleanup back to fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,7 +1,3 @@
-if command -v starship >/dev/null
-    starship init fish | source
-end
-
 for path in (ls -a ~/.config/fish/config.fish.*);
     source $path
 end
--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ set -x
 
 
 if [[ "$CODESPACES" = "true" ]]; then
-  rm ~/.bashrc
   sudo apt update
   sudo apt-get install -y rcm tmux universal-ctags wget fuse
   rcup -f -v -d . -t development -t github
@@ -17,11 +16,6 @@ if [[ "$CODESPACES" = "true" ]]; then
   chmod +x /usr/local/bin/rdm
   gh config set browser "rdm open"
 
-  sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-
-  export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH
-  brew install neovim tree-sitter
-
 elif [[ "$(uname)" = "Darwin" ]]; then
   brew install rcm
   rcup -v -d . -t macos -t development -t gpg
@@ -30,12 +24,6 @@ elif [[ "$(uname)" = "Darwin" ]]; then
 else
   >&2 echo "error: Unknown system"
   exit 1
-fi
-
-if command -v go &>/dev/null; then
-  go install github.com/nametake/golangci-lint-langserver@latest
-else
-  echo "Could not install golangci-lint-langserver. Go is not installed or not in PATH."
 fi
 
 if [[ "$CODESPACES" = "true" ]]; then
