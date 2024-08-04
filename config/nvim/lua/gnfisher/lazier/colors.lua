e26c49acef5f54a6375ff5778d61ae4bab3b31fa
Greg Fisher <greg@gnfisher.com>
2024-08-04 13:03:16 -0400

    change

--- config/nvim/lua/gnfisher/lazier/colors.lua
+++ config/nvim/lua/gnfisher/lazier/colors.lua
@@ -1,26 +1,33 @@
 return {
-  'projekt0n/github-nvim-theme',
+  'jonathanfilip/vim-lucius',
+  -- 'projekt0n/github-nvim-theme',
   -- 'akiicat/vim-github-theme',
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   -- priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    require("github-theme").setup({
-      options = {
-        hide_end_of_buffer = false,
-        dim_inactive = true,
-        styles = {
-          comments = "italic",
-          functions = "bold",
-        },
-      },
-    })
+  init = function()
+    -- require("github-theme").setup({
+    --   options = {
+    --     hide_end_of_buffer = false,
+    --     dim_inactive = true,
+    --     styles = {
+    --       comments = "italic",
+    --       functions = "bold",
+    --     },
+    --   },
+    -- })
 
-    -- vim.cmd('colorscheme github_dark_dimmed')
+    vim.cmd('colorscheme lucius')
     local function ToggleBg()
       if vim.o.background == "dark" then
         vim.o.background = "light"
+        vim.g.lucius_style = "light"
+        vim.g.lucius_contrast = "high"
+        vim.g.lucius_contrast_bg = "normal"
         -- vim.cmd("colorscheme github_light")
       else
+        vim.g.lucius_style = "dark"
+        vim.g.lucius_contrast = "high"
+        vim.g.lucius_contrast_bg = "normal"
         vim.o.background = "dark"
         -- vim.cmd("colorscheme github_dark_dimmed")
       end
