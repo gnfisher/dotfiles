aa4385a1e890cb8c4d9851eb9b839febae6c4e52
Greg Fisher <greg@gnfisher.com>
2024-08-10 22:00:45 -0400

    Another set of fixes

--- config/nvim/lua/gnfisher/lazier/copilot.lua
+++ config/nvim/lua/gnfisher/lazier/copilot.lua
@@ -3,7 +3,6 @@ return {
   config = function()
     vim.cmd(":Copilot disable")
     vim.keymap.set("i", "<C-j>", "<Plug>(copilot-suggest)")
-    vim.keymap.set("i", "<C-]>", "<Plug>(copilot-next)")
-    vim.keymap.set("i", "<C-[>", "<Plug>(copilot-previous)")
+    vim.keymap.set("i", "<C-k>", "<Plug>(copilot-next)")
   end
 }
