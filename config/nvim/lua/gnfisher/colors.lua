42e6100276883d88ea61be8937d60e10aa5d9369
Greg Fisher <greg@gnfisher.com>
2024-09-05 19:09:44 -0400

    Darkness

--- config/nvim/lua/gnfisher/colors.lua
+++ config/nvim/lua/gnfisher/colors.lua
@@ -1,19 +1,18 @@
 local function SetDark()
   vim.o.background = "dark"
-  vim.cmd.colorscheme("gruvbuddy")
+  vim.cmd.colorscheme("github_dark")
 end
 
 function ToggleBg()
   if vim.o.background == "dark" then
     vim.o.background = "light"
-    vim.cmd.colorscheme("delek")
+    vim.cmd.colorscheme("github_light")
   else
     SetDark()
   end
 end
 
-vim.o.background = "light"
-vim.cmd.colorscheme("delek")
+vim.cmd.colorscheme("github_dark")
 vim.keymap.set('n', '<F6>', ToggleBg, { noremap = true, silent = true })
 
 vim.cmd('highlight clear MatchParen')
