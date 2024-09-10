b3a5cea8796f2e118e80d931ed3a74d027b22c84
Greg Fisher <greg@gnfisher.com>
2024-09-10 19:45:38 -0400

    Some things have changed

--- config/nvim/lua/gnfisher/colors.lua
+++ config/nvim/lua/gnfisher/colors.lua
@@ -1,18 +1,18 @@
 local function SetDark()
   vim.o.background = "dark"
-  vim.cmd.colorscheme("github_dark")
+  vim.cmd.colorscheme("kanagawa")
 end
 
 function ToggleBg()
   if vim.o.background == "dark" then
     vim.o.background = "light"
-    vim.cmd.colorscheme("github_light")
+    vim.cmd.colorscheme("kanagawa-lotus")
   else
     SetDark()
   end
 end
 
-vim.cmd.colorscheme("github_dark")
+vim.cmd.colorscheme("kanagawa")
 vim.keymap.set('n', '<F6>', ToggleBg, { noremap = true, silent = true })
 
 vim.cmd('highlight clear MatchParen')
--- config/nvim/lua/gnfisher/lazier/colors.lua
+++ config/nvim/lua/gnfisher/lazier/colors.lua
@@ -8,4 +8,5 @@ return {
     end
   },
   { 'projekt0n/github-nvim-theme' },
+  { 'rebelot/kanagawa.nvim' },
 }
