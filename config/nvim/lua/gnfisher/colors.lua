48d31538082365b943f974501642ef85ad7001d1
Greg Fisher <greg@gnfisher.com>
2024-09-18 20:18:21 -0400

    no

--- config/nvim/lua/gnfisher/colors.lua
+++ config/nvim/lua/gnfisher/colors.lua
@@ -1,16 +1,14 @@
 local function SetDark()
   vim.o.background = "dark"
-  vim.cmd.colorscheme("solarized")
 end
 
 function ToggleBg()
   if vim.o.background == "dark" then
     vim.o.background = "light"
-    vim.cmd.colorscheme("solarized")
   else
     SetDark()
   end
 end
 
-vim.cmd.colorscheme("solarized")
+vim.cmd.colorscheme("default")
 vim.keymap.set('n', '<F6>', ToggleBg, { noremap = true, silent = true })
