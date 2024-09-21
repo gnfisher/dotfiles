a9c9323960284cc935bc268d806665879bf55c96
Greg Fisher <greg@gnfisher.com>
2024-09-21 18:57:46 -0400

    yea

--- config/nvim/lua/gnfisher/lazier/colors.lua
+++ config/nvim/lua/gnfisher/lazier/colors.lua
@@ -1,5 +1,5 @@
 local function SetColor(color)
-  color = color or "rose-pine-moon"
+  color = color or "rose-pine-dawn"
   vim.cmd.colorscheme(color)
   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
