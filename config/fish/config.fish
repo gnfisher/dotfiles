0d9c806809faf0df42c4b84730318622174ae032
Greg Fisher <greg@gnfisher.com>
2024-09-21 22:33:50 -0400

    the final form

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,5 +5,4 @@ end
 set -x EDITOR "nvim"
 set -x VISUAL "nvim"
 
-# fish_config theme choose "solarized dark"
 fish_config prompt choose "arrow"
--- config/nvim/lua/gnfisher/lazier/colors.lua
+++ config/nvim/lua/gnfisher/lazier/colors.lua
@@ -1,10 +1,11 @@
 local function SetColor(color)
-  color = color or "rose-pine-dawn"
+  color = color or "rose-pine-moon"
   vim.cmd.colorscheme(color)
   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
   vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
   vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
   vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { bg = "none" })
   vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { bg = "none" })
   vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "none" })
