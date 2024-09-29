a43ae7046b8558c980eef6e3be83a6a667dc374c
Greg Fisher <greg@gnfisher.com>
2024-09-29 08:28:34 -0400

    Final?

--- config/nvim/lua/gnfisher/lazier/colors.lua
+++ config/nvim/lua/gnfisher/lazier/colors.lua
@@ -16,4 +16,24 @@ return {
       SetColor("tokyonight-storm")
     end
   },
+  {
+    'projekt0n/github-nvim-theme',
+    lazy = false,
+    config = function()
+      require('github-theme').setup({
+        styles = {           -- Style to be applied to different syntax groups
+          comments = 'NONE', -- Value is any valid attr-list value `:help attr-list`
+          functions = 'bold',
+          keywords = 'NONE',
+          variables = 'NONE',
+          conditionals = 'NONE',
+          constants = 'NONE',
+          numbers = 'NONE',
+          operators = 'NONE',
+          strings = 'NONE',
+          types = 'bold',
+        },
+      })
+    end,
+  },
 }
--- config/nvim/lua/gnfisher/toggle_bg.lua
+++ config/nvim/lua/gnfisher/toggle_bg.lua
@@ -1,3 +1,4 @@
+-- Create a command to toggle between dark/light background
 vim.api.nvim_create_user_command("ToggleBg", function()
   if vim.o.background == "dark" then
     vim.o.background = "light"
