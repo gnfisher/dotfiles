49502757b18e3b1f1b603a420860c3546f8dfe8e
Greg Fisher <greg@gnfisher.com>
2024-10-13 11:48:41 -0400

    soon

--- config/nvim/lua/gnfisher/lazier/oil.lua
+++ config/nvim/lua/gnfisher/lazier/oil.lua
@@ -5,9 +5,11 @@ return {
   opts = {},
   -- Optional dependencies
   -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
   config = function()
-    require("oil").setup()
+    require("oil").setup({
+      columns = {},
+    })
     vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
   end,
 }
