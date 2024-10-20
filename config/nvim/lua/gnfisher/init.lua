04b9f81b90651f64bbcfdc7e5881718c17e9c2b9
Greg Fisher <greg@gnfisher.com>
2024-10-20 17:48:17 -0400

    remove unused stuff

--- config/nvim/lua/gnfisher/init.lua
+++ config/nvim/lua/gnfisher/init.lua
@@ -1,14 +1,10 @@
 vim.g.mapleader = " "
 
 require("gnfisher.set")
--- require("gnfisher.lazy_init")
 require("gnfisher.keymaps")
 
 require("gnfisher.close_help_window")
--- require("gnfisher.fugitive_cmd")
--- require("gnfisher.fugitive_toggle")
 require("gnfisher.highlight_yank")
 require("gnfisher.jump_to_last_line")
--- require("gnfisher.statusline")
 require("gnfisher.toggle_bg")
 require("gnfisher.trim_whitespace")
