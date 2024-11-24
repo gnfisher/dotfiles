38c91ab02a5f7f736eb7e02c341a4fe0ad10a585
Greg Fisher <greg@gnfisher.com>
2024-11-24 10:57:35 -0500

    Adjustments

--- install.sh
+++ install.sh
@@ -4,17 +4,16 @@ exec > >(tee -i $HOME/dotfiles_install.log)
 exec 2>&1
 set -x
 
-
 if [[ "$CODESPACES" = "true" ]]; then
   sudo apt update
-  sudo apt-get install -y rcm universal-ctags wget
+  sudo apt-get install -y rcm wget
   rcup -f -v -d . -t development -t github
 
   # Use RDM for copy/paste and open support
-  wget https://github.com/BlakeWilliams/remote-development-manager/releases/download/v0.0.3/rdm-linux-amd64
-  sudo mv rdm-linux-amd64 /usr/local/bin/rdm
-  chmod +x /usr/local/bin/rdm
-  gh config set browser "rdm open"
+  # wget https://github.com/BlakeWilliams/remote-development-manager/releases/download/v0.0.3/rdm-linux-amd64
+  # sudo mv rdm-linux-amd64 /usr/local/bin/rdm
+  # chmod +x /usr/local/bin/rdm
+  # gh config set browser "rdm open"
 
 elif [[ "$(uname)" = "Darwin" ]]; then
   brew install rcm
