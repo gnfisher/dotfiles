7774bfba4995b5e64d085c88277acfe5b7f5722e
Greg Fisher <greg@gnfisher.com>
2025-01-26 16:23:22 -0500

    colors more

--- tag-development/config/nvim/lua/gnfisher/lazier/colors.lua
+++ tag-development/config/nvim/lua/gnfisher/lazier/colors.lua
@@ -24,7 +24,7 @@ return {
       require("gruvbox").setup({
         terminal_colors = true, -- add neovim terminal colors
         undercurl = true,
-        underline = false,
+        underline = true,
         bold = true,
         italic = {
           strings = false,
@@ -39,11 +39,11 @@ return {
         invert_tabline = false,
         invert_intend_guides = false,
         inverse = true, -- invert background for search, diffs, statuslines and errors
-        contrast = "",  -- can be "hard", "soft" or empty string
+        contrast = "hard",  -- can be "hard", "soft" or empty string
         palette_overrides = {},
         overrides = {},
         dim_inactive = false,
-        transparent_mode = true,
+        transparent_mode = false,
       })
       ColorMyPencils()
     end,
